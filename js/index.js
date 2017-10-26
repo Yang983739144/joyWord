6ede6b791ff08acc30df418adb3129f1ef3b4faf
Yang983739144 <983739144@qq.com>
2017-10-26 10:03:54 +0800

    初版

--- js/index.js
+++ js/index.js
@@ -2,4 +2,17 @@ $(function(){
 	$(".shop").hover(function(){
 		$("#tan").toggleClass("active");
 	});
+	$("#left>ul>li").mouseenter(function(){
+	   let index = $(this).index();
+	   $(this).css("background","white");
+	   $(this).find(".left-2-1").css("color","black");  
+	   $($(".left-2-1")[index]).siblings(".game1").css("display","block");
+	});
+	$("#left>ul>li").mouseout(function(){
+	   let index = $(this).index();
+	   $(this).css("background","#f55656");
+	   $(this).find(".left-2-1").css("color","white");
+	    $($(".left-2-1")[index]).siblings(".game1").css("display","none");
+	});
 })
+
